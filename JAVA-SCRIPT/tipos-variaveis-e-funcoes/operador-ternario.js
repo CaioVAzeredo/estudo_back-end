42315fa7c21ed2a7fbb16308954ab59f9896fd59
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-09-27 22:53:18 -0300

    atividade feita

---
+++ JAVA-SCRIPT/tipos-variaveis-e-funcoes/operador-ternario.js
@@ -0,0 +1,6 @@
+const idadeMinima = 18
+const idadeCliente = 16
+
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+
+//usar o if por ser melhor para fazer manutenção 
---
+++ JAVA-SCRIPT/tipos-variaveis-e-funcoes/template-strings.js
@@ -0,0 +1,6 @@
+const nome = "caio"
+const idade = 2023 - 1998
+const cidadeDeNascimento = "Brasilia"
+const apresentacao = `meu noem é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao)
