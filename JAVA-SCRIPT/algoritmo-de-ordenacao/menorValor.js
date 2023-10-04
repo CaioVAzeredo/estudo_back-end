d4b529a8da1fa24dd773f3b6f07c7138be2adfa0
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-10-04 11:38:14 -0300

    aula algoritmo

---
+++ JAVA-SCRIPT/algoritmo-de-ordenacao/menorValor.js
@@ -0,0 +1,16 @@
+const livros = require('./livros')
+
+function menorValor(arrProduto, posicaoInicial){
+
+    let maisBarato = posicaoInicial;
+    
+    for (let atual = posicaoInicial; atual < arrProduto.length; atual++) {
+        if (arrProduto[atual].preco < arrProduto[maisBarato].preco) {
+            maisBarato = atual
+        }
+    }
+    return maisBarato
+}
+
+
+module.exports = menorValor()
---
+++ JAVA-SCRIPT/algoritmo-de-ordenacao/selectionSort.js
@@ -0,0 +1,8 @@
+const livros = require('./livros')
+const menorValor = require('./menorValor')
+
+for(let atual = 0; atual < livros.length; atual++){
+    
+}
+
+console.log(menorValor)
