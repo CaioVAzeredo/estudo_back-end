8d081973c4d8148e38819d583973eaf800f67ec3
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-10-31 22:45:44 -0300

    atividade em andamento

--- JAVA-SCRIPT/criando-sua-primeira-biblioteca/index.js
+++ JAVA-SCRIPT/criando-sua-primeira-biblioteca/index.js
@@ -1,21 +1,37 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-function trataErro(erro) {
-    console.log(erro);
-    throw new Error(chalk.red(erro.code, 'Nao há arquivo no diretório'));
-
-}
-
 //async/await
-
-function pegaArquivo(caminhoDoArquivo){
-    
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(extraiLinks(chalk.green(texto)));
+    } catch (erro) {
+        trataErro(erro);
+    }
 }
 
 
-//promises com then()
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; //expressão regular
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }))
 
+    return resultados;
+}
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Nao há arquivo no diretório'));
+}
+
+
+
+pegaArquivo('./arquivos/texto.md');
+
+
+/* --------------------------------------------------------------------------------------- */
+//promises com then() - pode ser substituido no lugar de async/await
 /* function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     fs.promises
@@ -23,6 +39,3 @@ function pegaArquivo(caminhoDoArquivo){
         .then((texto) => console.log(chalk.green(texto)))
         .catch(trataErro);
 } */
-
-
-pegaArquivo('./arquivos/');
