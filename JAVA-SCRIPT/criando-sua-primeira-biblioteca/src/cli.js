cc9a810f040afd5b896e2ef6da6871ee7b95a4e8
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-11-01 21:34:21 -0300

    quarta aula finalizada

--- JAVA-SCRIPT/criando-sua-primeira-biblioteca/src/cli.js
+++ JAVA-SCRIPT/criando-sua-primeira-biblioteca/src/cli.js
@@ -1,4 +1,38 @@
-import pegaArquivo from './index.js'
+import chalk from 'chalk';
+import fs from 'fs';
+import pegaArquivo from './index.js';
 
-const caminho = process.argv;
-pegaArquivo(caminho[2]);
+const caminho = process.argv; //Um Array que está armazenando argumentos de linha de comando 
+
+async function processaTexto(argumento) {
+    const caminho = argumento[2];
+
+    try {
+        fs.lstatSync(caminho);//obter informações sobre um arquivo ou diretório no sistema de arquivos
+    } catch (erro) {
+        if (erro.code === 'ENOENT') {
+            console.log(chalk.red('arquivo ou diretório não existe'));
+            return
+        }
+    }
+
+    if (fs.lstatSync(caminho).isFile()) { //se a informação do caminho for um arquivo
+        const resultado = await pegaArquivo(argumento[2]); // leva o arquivo para ser tratado //passando como argumento a terceira posição do array
+        imprimeLista(resultado); // resultado do tratamento do resultado 
+    } else if (fs.lstatSync(caminho).isDirectory()) { // se a informação do caminho for um diretório
+        const arquivos = await fs.promises.readdir(caminho); // vai ler o diretório(.readdir()) colocado como argumento como uma promessa e retornar em uma array
+
+        arquivos.forEach(async (nomeDeArquivo) => { // percorrer pela array 
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`);// leva o arquivo para ser tratado
+            imprimeLista(nomeDeArquivo, lista); // resultado do tratamento do resultado 
+        })
+    }
+}
+
+function imprimeLista(nomeDeArquivo = '', resultado) {
+    console.log(chalk.yellow(`Lista de links`), chalk.black.bgGreen(nomeDeArquivo), resultado)
+}
+
+
+
+processaTexto(caminho)
--- JAVA-SCRIPT/criando-sua-primeira-biblioteca/src/index.js
+++ JAVA-SCRIPT/criando-sua-primeira-biblioteca/src/index.js
@@ -6,7 +6,7 @@ async function pegaArquivo(caminhoDoArquivo) {
     try {
         const encoding = 'utf-8';
         const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-        console.log(extraiLinks(chalk.green(texto)));
+        return extraiLinks(texto);
     } catch (erro) {
         trataErro(erro);
     }
@@ -16,9 +16,9 @@ async function pegaArquivo(caminhoDoArquivo) {
 function extraiLinks(texto) {
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm; //expressão regular
     const capturas = [...texto.matchAll(regex)];
-    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }))
+    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }));
 
-    return resultados;
+    return resultados.length !== 0 ? resultados : 'Não há links no arquivo';
 }
 
 function trataErro(erro) {
