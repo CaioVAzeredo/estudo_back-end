2695bbe4f84891a051fa6ef84035f6e0f4ecd466
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-11-08 23:00:12 -0300

    curso de POO finalizado

--- JAVA-SCRIPT/programacao-orientada-a-objeto/Admin.js
+++ JAVA-SCRIPT/programacao-orientada-a-objeto/Admin.js
@@ -4,8 +4,13 @@ export default class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo); //super() serve para pegar as caracteristicas da class que foi extendida
     }
+    
     criarCurso(nomeDoCurso, vagas){
         return`Curso de ${nomeDoCurso} criado com ${vagas} vagas`;
+    }
+
+    exibirInfos(){
+        return `${this.nome}, ${this.ativo}`
     }
 }
 
--- JAVA-SCRIPT/programacao-orientada-a-objeto/User.js
+++ JAVA-SCRIPT/programacao-orientada-a-objeto/User.js
@@ -1,4 +1,5 @@
-export default class User {7
+export default class User {
+    7
     #nome; // o # serve para privar a variável 
     #email;
     #nascimento;
@@ -11,9 +12,57 @@ export default class User {7
         this.#role = role || 'estudante';
         this.#ativo = ativo;
     }
-    exibirInfos() {
-        return `${this.#nome}, ${this.#email}`
+
+    get nome() {
+        //Pode ter lógica como if
+        return this.#nome;
     }
+
+    get email() {
+        //Pode ter lógica como if
+        return this.#email;
+    }
+
+    get nascimento() {
+        //Pode ter lógica como if
+        return this.#nascimento;
+    }
+
+    get ativo() {
+        //Pode ter lógica como if
+        return this.#ativo;
+    }
+
+    get role() {
+        //Pode ter lógica como if
+        return this.#role;
+    }
+    
+    set nome(novoNome){
+        if(novoNome ===''){
+            throw new Error('Formato não válido');
+        }
+        this.#nome = novoNome;
+    }
+
+    /*          usado apenas como exemplo
+            
+    #montaObjUser() { //função privada, não conseguimos acessar de fora do objeto 
+        return ({
+            nome: this.#nome,
+            email: this.#email,
+            nascimento: this.#nascimento,
+            role: this.#role,
+            ativo: this.#ativo
+        })
+    } */
+
+    exibirInfos() {
+        /* const objUser = this.#montaObjUser()  */// forma de acessar uma função de fora é passando para uma variável
+       /*  return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}` // se refere à variavel objUser */
+        return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}` //acessando sem a # pois se refere aos getters 
+    }
+
 }
 
 
