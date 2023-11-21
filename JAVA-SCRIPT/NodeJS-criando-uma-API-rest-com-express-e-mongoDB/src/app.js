f3e437b9caeb72282ce630241cef53d920d50547
CaioVAzeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-11-21 16:29:15 -0300

    feito o app.post

--- JAVA-SCRIPT/NodeJS-criando-uma-API-rest-com-express-e-mongoDB/src/app.js
+++ JAVA-SCRIPT/NodeJS-criando-uma-API-rest-com-express-e-mongoDB/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); //middleware -> ter acesso as requisições no momento em que elas estao sendo feitas 
+
 const livros = [
     {
         id: 1,
@@ -19,4 +21,10 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
+
+app.post("/livros", (req, res) =>{
+    livros.push(req.body);
+    res.status(201).send("livro cadastrado com sucesso!") // 201 -> codigo de status http de registro enviado 
+})
+
 export default app;
