92a9cc66fe7197da72fd22352869cf7c5d1f4a02
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-11-21 11:21:42 -0300

    feito get livros

--- JAVA-SCRIPT/NodeJS-criando-uma-API-rest-com-express-e-mongoDB/src/app.js
+++ JAVA-SCRIPT/NodeJS-criando-uma-API-rest-com-express-e-mongoDB/src/app.js
@@ -1,3 +1,22 @@
 import express from "express";
 
 const app = express();
+const livros = [
+    {
+        id: 1,
+        titulo: "HP"
+    },
+    {
+        id: 2,
+        titulo: "IT: A coisa"
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+export default app;
