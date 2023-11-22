6dc1adbef0f6cd0b7fcf015c00e439db9d26f4c2
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2023-11-21 22:23:14 -0300

    aula finalizada

--- JAVA-SCRIPT/NodeJS-criando-uma-API-rest-com-express-e-mongoDB/src/app.js
+++ JAVA-SCRIPT/NodeJS-criando-uma-API-rest-com-express-e-mongoDB/src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 
+// get: pegar
+// post: criar
+// put: alterar 
+
+//Metodo CRUD = Create, Read, Update, Delete
+
 const app = express();
 app.use(express.json()); //middleware -> ter acesso as requisições no momento em que elas estao sendo feitas 
-
+/* ------------------------------------------ */
 const livros = [
     {
         id: 1,
@@ -12,8 +18,14 @@ const livros = [
         id: 2,
         titulo: "IT: A coisa"
     }
-]
+];
 
+function buscaLivros(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+/* ------------------------------------------ */
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
@@ -22,9 +34,26 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
-app.post("/livros", (req, res) =>{
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    res.status(200).json(livros[index])
+})
+/* ------------------------------------------ */
+app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("livro cadastrado com sucesso!") // 201 -> codigo de status http de registro enviado 
+})
+/* ------------------------------------------ */
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros); // json() retorna um objeto
+})
+/* ------------------------------------------ */
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivros(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso"); 
 })
 
 export default app;
