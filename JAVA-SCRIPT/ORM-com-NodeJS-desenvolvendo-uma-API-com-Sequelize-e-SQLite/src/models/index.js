7d7896bb30b55b4472cd0922887b7ea972f8ab29
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-01-19 15:08:49 -0300

    feita a conexao com o banco

--- JAVA-SCRIPT/ORM-com-NodeJS-desenvolvendo-uma-API-com-Sequelize-e-SQLite/src/models/index.js
+++ JAVA-SCRIPT/ORM-com-NodeJS-desenvolvendo-uma-API-com-Sequelize-e-SQLite/src/models/index.js
@@ -5,17 +5,20 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
+
+//Verificar qual é o ambiente de execução e as credenciais de acesso ao banco correspondentes. Já existe um código pronto para buscar o ambiente em um arquivo .env ou, na falta dele, assumir que o ambiente a ser utilizado é development:
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+//Criar uma nova instância de Sequelize a partir dos dados de conexão:
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
+//Percorrer a pasta models, indexar e executar o código de cada modelo dentro da pasta e salvar o objeto resultante na variável db:
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -31,6 +34,7 @@ fs
     db[model.name] = model;
   });
 
+//Indexar quais modelos estão associados entre si e os métodos correspondentes (iremos falar de associações durante este curso):
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
