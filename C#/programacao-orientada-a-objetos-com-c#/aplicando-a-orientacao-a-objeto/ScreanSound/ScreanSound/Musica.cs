abfe14374e9ddc9af597b52e6e200568eef063c4
Caio Viana <caio.viana.39@gmail.com>
2024-08-28 22:04:50 -0300

    refazendo a atividade

---
+++ C#/programacao-orientada-a-objetos-com-c#/aplicando-a-orientacao-a-objeto/ScreanSound/ScreanSound/Musica.cs
@@ -0,0 +1,21 @@
+﻿class Musica{
+   public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+
+    public void ExibirFichaTecnica()
+    {
+
+        Console.WriteLine($"\nNome musica: {this.nome}\nArtista: {this.artista}\nDuração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano");
+        }
+    }
+}
