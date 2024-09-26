d7d1239ac8b7b9d303c262d45f9e8b55b98b6afc
Caio Viana <caio.viana.39@gmail.com>
2024-09-26 20:47:52 -0300

    Atividade feita

---
+++ C#/atividades/atividade8/Produto/Produto/Produto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using (HttpClient client = new HttpClient())
+{
+    Dictionary<string, double> produtos = new Dictionary<string, double>
+        {
+            { "Camiseta", 29.99 },
+            { "Calça Jeans", 79.90 },
+            { "Tênis", 199.90 },
+            { "Boné", 39.90 }
+        };
+
+    string produtosString = JsonSerializer.Serialize(produtos); 
+    var produtosEmString = JsonSerializer.Deserialize<string>(produtosString);
+
+    Console.WriteLine(produtosEmString);
+}
