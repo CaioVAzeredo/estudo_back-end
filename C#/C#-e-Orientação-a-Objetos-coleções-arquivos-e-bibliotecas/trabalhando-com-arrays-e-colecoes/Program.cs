97a4fe083aa1be2f01adc23760464149d46467dc
Caio Viana de Azeredo <122998245+CaioVAzeredo@users.noreply.github.com>
2024-10-01 12:51:11 -0300

    atividade em andamento

---
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arrays-e-colecoes/Program.cs
@@ -0,0 +1,72 @@
+﻿//ArrayInt();
+//TestaBuscaPalavra();
+void ArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        System.Console.WriteLine($"Ídades[{i}] = {idade}");
+        acumulador += idade;
+    }
+    int media = acumulador / idades.Length;
+    System.Console.WriteLine($"A média é: {media}");
+}
+void TestaBuscaPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.WriteLine($"Digite {i + 1}° Palavra:   ");
+        arrayDePalavras[i] = Console.ReadLine()!;
+
+    }
+    Console.WriteLine("Digite palavra a se encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
+    }
+}
+
+Array amostra = new double[5];/* Array.CreateInstance(typeof(double), 5); */
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if (array == null || array.Length == 0)
+    {
+        System.Console.WriteLine("Array para calculo mediana está vazio ou nulo");
+    }
+
+    double[] numerosOrdenados = (double[])array!.Clone(); //(double[])array = convertendo o objeto array para um array de double[]. Clone() = clona o objeto
+    Array.Sort(numerosOrdenados); // Ordena em ordem decrescente
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+    System.Console.WriteLine($"A mediana é de: {mediana}");
+
+}
