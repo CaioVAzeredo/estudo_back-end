5f330e751d68deb07538546af37c6daf92b691a3
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-01-13 17:30:41 -0300

    estudo em andamento

---
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/1_LidandoComFileSreamDiretamente.cs
@@ -0,0 +1,46 @@
+﻿using ByteBankIO;
+using System.Text;
+
+partial class Program
+{
+    static void LidandoComFileStreamDiretamente()
+    {
+        var enderecoDoArquivo = "contas.txt";
+
+        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var numerosDeBytesLidos = -1;
+            var buffer = new byte[1024]; //1KB
+
+            while (numerosDeBytesLidos != 0)
+            {
+                numerosDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); //public override int Read(byte[] array, int offset, int count);
+
+                Console.WriteLine($"Bytes lidos: {numerosDeBytesLidos}");
+                EscreverBuffer(buffer, numerosDeBytesLidos);
+
+            }
+
+            fluxoDoArquivo.Close(); //Conseguir alterar mudanças com o arquivo em uso
+            Console.ReadLine();
+        }
+
+
+    }
+
+    static void EscreverBuffer(byte[] buffer, int bytesLidos)
+    {
+        var utf8 = new UTF8Encoding();
+
+        var texto = utf8.GetString(buffer, 0, bytesLidos); //public virtual string GetString(bytes[] bytes, int index, int count);
+
+        Console.Write(texto);
+        /*
+        foreach (var meuByte in buffer)
+        {
+            Console.WriteLine(meuByte);
+            Console.WriteLine(" ");
+        }
+        */
+    }
+}
--- C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/Program.cs
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/Program.cs
@@ -1,37 +1,29 @@
 ﻿using ByteBankIO;
 using System.Text;
 
-class Program
+partial class Program
 {
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
-        var numerosDeBytesLidos = -1;
 
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-        var buffer = new byte[1024]; //1KB
-
-        while (numerosDeBytesLidos != 0)
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
         {
-            numerosDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); //public override int Read(byte[] array, int offset, int count);
-            EscreverBuffer(buffer);
+            var leitor = new StreamReader(fluxoDeArquivo);
 
+            //var linha = leitor.ReadLine(); //ler a primeira linha
+            //var text = leitor.ReadToEnd(); //leitura total 
+
+            //int num = leitor.Read(); // retorna o primeiro exadecimal 
+
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                Console.WriteLine(linha);
+            }
         }
+
+        Console.ReadLine();
     }
 
-    static void EscreverBuffer(byte[] buffer)
-    {
-        var utf8 = new UTF8Encoding();
-        
-        var texto = utf8.GetString(buffer);
-        Console.Write(texto);
-        /*
-        foreach (var meuByte in buffer)
-        {
-            Console.WriteLine(meuByte);
-            Console.WriteLine(" ");
-        }
-        */
-    }
 }
