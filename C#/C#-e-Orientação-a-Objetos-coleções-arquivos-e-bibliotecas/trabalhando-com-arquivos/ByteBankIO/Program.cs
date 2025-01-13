04fd04205e8a10019210cd2dfa09b9da303dda5c
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-01-13 13:52:32 -0300

    começo de um novo estudo

--- C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/Program.cs
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/Program.cs
@@ -1,10 +1,37 @@
 ﻿using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var numerosDeBytesLidos = -1;
 
-        Console.ReadLine();
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        while (numerosDeBytesLidos != 0)
+        {
+            numerosDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024); //public override int Read(byte[] array, int offset, int count);
+            EscreverBuffer(buffer);
+
+        }
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        var utf8 = new UTF8Encoding();
+        
+        var texto = utf8.GetString(buffer);
+        Console.Write(texto);
+        /*
+        foreach (var meuByte in buffer)
+        {
+            Console.WriteLine(meuByte);
+            Console.WriteLine(" ");
+        }
+        */
     }
 }
