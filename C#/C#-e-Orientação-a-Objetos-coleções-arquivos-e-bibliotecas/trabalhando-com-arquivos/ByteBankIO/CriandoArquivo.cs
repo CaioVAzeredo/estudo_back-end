e0a8cbdf0a01636144243f679313fe1fc9989cb2
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-01-17 14:40:59 -0300

    atividades em andamento

---
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/CriandoArquivo.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using(var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7895, 4785,40, Caio Viana";
+
+            var bytes = Encoding.UTF8.GetBytes(contaComoString);
+            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+
+        }
+    }
+}
+
--- C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/Program.cs
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/Program.cs
@@ -5,25 +5,57 @@ partial class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt";
-
-        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-        {
-            var leitor = new StreamReader(fluxoDeArquivo);
-
-            //var linha = leitor.ReadLine(); //ler a primeira linha
-            //var text = leitor.ReadToEnd(); //leitura total 
-
-            //int num = leitor.Read(); // retorna o primeiro exadecimal 
-
-            while (!leitor.EndOfStream)
-            {
-                var linha = leitor.ReadLine();
-                Console.WriteLine(linha);
-            }
-        }
-
+        CriarArquivo();
         Console.ReadLine();
+        //    var enderecoDoArquivo = "contas.txt";
+
+        //    using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        //    {
+        //        var leitor = new StreamReader(fluxoDeArquivo);
+
+        //        //var linha = leitor.ReadLine(); //ler a primeira linha
+        //        //var text = leitor.ReadToEnd(); //leitura total 
+
+        //        //int num = leitor.Read(); // retorna o primeiro exadecimal 
+
+        //        while (!leitor.EndOfStream)
+        //        {
+        //            var linha = leitor.ReadLine();
+        //            Console.WriteLine(linha);
+
+        //            var contaCorrente = ConverterStringParaContaCorrente(linha);
+
+        //            var msg = $"Conta número: {contaCorrente.Numero}, ag: {contaCorrente.Agencia}, saldo:{contaCorrente.Saldo}";
+        //            Console.WriteLine(msg);
+        //        }
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        //static ContaCorrente ConverterStringParaContaCorrente(string? linha)
+        //{
+
+        //    var campos = linha.Split(',');
+
+        //    var agencia = campos[0];
+        //    var numero = campos[1];
+        //    var saldo = campos[2].Replace('.', ','); // Replace serve para mudar uma coisa por outra
+        //    var nomeTitular = campos[3];
+
+        //    var agenciaComInt = int.Parse(agencia);
+        //    var numeroComInt = int.Parse(numero);
+        //    var saldoComDouble = double.Parse(saldo);
+
+        //    var titular = new Cliente();
+        //    titular.Nome = nomeTitular;
+
+        //    var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        //    resultado.Depositar(saldoComDouble);
+        //    resultado.Titular = titular;
+
+        //    return resultado;
+
     }
 
 }
