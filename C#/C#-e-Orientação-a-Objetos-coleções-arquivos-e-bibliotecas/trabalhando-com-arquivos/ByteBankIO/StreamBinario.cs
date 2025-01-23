3ec95f80a5708399fa7ec8532c3b94399d9c2a92
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-01-22 21:09:56 -0300

    concluído o curso

---
+++ C#/C#-e-Orientação-a-Objetos-coleções-arquivos-e-bibliotecas/trabalhando-com-arquivos/ByteBankIO/StreamBinario.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void EscritaBinario()
+    {
+
+        using (var fs = new FileStream("contasExportadas.txt", FileMode.Create))
+        using (var escritor = new BinaryWriter(fs))
+        {
+            escritor.Write(456);
+            escritor.Write(45665);
+            escritor.Write(4000.50);
+            escritor.Write("Caio Viana");
+        }
+
+    }
+        static void LeituraBinaria()
+        {
+            using (var fs = new FileStream("contasExportadas.txt", FileMode.Open))
+            using (var leitor = new BinaryReader(fs))
+            {
+                var agencia = leitor.ReadInt32();
+                var numeroConta = leitor.ReadInt32();
+                var saldo = leitor.ReadDouble();
+                var titular = leitor.ReadString();
+
+                Console.WriteLine($"{agencia}/{numeroConta} {titular} {saldo}");
+            }
+        }
+}
