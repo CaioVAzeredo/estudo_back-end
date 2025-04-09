5a4b0ee47ab22902ab95ee7de97fe9954418e887
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-04-09 09:55:47 -0300

    Adição do stream

--- JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/trabalhando-com-lambda/screenmatch/src/main/java/com/example/screenmatch/principal/Principal.java
+++ JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/trabalhando-com-lambda/screenmatch/src/main/java/com/example/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package com.example.screenmatch.principal;
 
+import com.example.screenmatch.model.DadosEpisodios;
 import com.example.screenmatch.model.DadosSeries;
 import com.example.screenmatch.model.DadosTemporada;
 import com.example.screenmatch.service.ConsumoApi;
@@ -8,6 +9,7 @@ import com.example.screenmatch.service.ConverteDados;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     Scanner leitura = new Scanner(System.in);
@@ -20,24 +22,33 @@ public class Principal {
     private final String API_KEY = "&apikey=6585022c";
 
 
-    public void exibeMenu(){
+    public void exibeMenu() {
         System.out.println("Digite o nome da serie para buscar");
         String nomeSerie = leitura.nextLine();
 
         //replace é passado dois parametros. o primeiro é o que tem e o segundo é para subistituir o do primeiro por esse. nesse caso vai trocar o espaço por +
-        String json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+")+ API_KEY);
+        String json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
 
         DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
         System.out.println(dados);
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for(int i =1; i<=dados.totalTemporadas(); i++){
-        json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+")+"&season"+i+ API_KEY);
-			DadosTemporada dadosTemporada= conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
 
-		temporadas.forEach(System.out::println);
+        temporadas.forEach(System.out::println);
+
+        //utilizando o lambda. bem semelhante a arrowfunction do JS. dessa vez utilizando o forEach
+        temporadas.forEach(temporada -> temporada.episodios().forEach(episodio -> System.out.println(episodio.titulo())));
+
+        List<DadosEpisodios> dadosEpisodios = temporadas.stream()
+                .flatMap(temporada -> temporada.episodios().stream()) //flatMap serve para juntar a lista da lista em uma só
+                .collect(Collectors.toList()); //o .collect(Collectors.toList()) tem a função de listar, essa lista é mutavel diferente do toList que é imutavel
+
+        dadosEpisodios.forEach(System.out::println);
     }
 }
