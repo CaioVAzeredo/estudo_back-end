5eb72ccadc881739a4ea2297756ce8923f54c733
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-05-19 10:15:43 -0300

    feito o GeneratedValue do id

--- JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/persistencia-de-dados-e-consulta/aula/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/persistencia-de-dados-e-consulta/aula/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -40,6 +40,14 @@ public class Serie {
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitulo() {
         return titulo;
     }
