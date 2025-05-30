0dafb5b26dc4cf851c377afbed5ecf0ebf8884f0
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-05-29 22:31:20 -0300

    adição do repository e service

--- JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/persistencia-de-dados-e-consulta/atividades/desafio/src/main/java/com/example/demo/DemoApplication.java
+++ JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/persistencia-de-dados-e-consulta/atividades/desafio/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.principal.Principal;
+import com.example.demo.repository.AtorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    private AtorRepository repositorio;
 
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal(repositorio);
+        principal.exibirMenu();
+    }
 }
---
+++ JAVA/CRIE-APLICACOES-USANDO-SPRING-BOOT/persistencia-de-dados-e-consulta/atividades/desafio/src/main/java/com/example/demo/principal/Principal.java
@@ -0,0 +1,15 @@
+package com.example.demo.principal;
+
+import com.example.demo.repository.AtorRepository;
+
+public class Principal {
+    private AtorRepository repositorio;
+
+    public Principal(AtorRepository repositorio) {
+        this.repositorio = repositorio;
+    }
+
+    public void exibirMenu() {
+
+    }
+}
